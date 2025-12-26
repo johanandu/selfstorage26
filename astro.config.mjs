acb1fe80d964d6d566e184df1e223bb6db12f255
johanandu <janek.durkowski@gmail.com>
2025-12-26 17:09:32 +0100

    version 1.0.0  1 Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  vite: {
-    define: {
-      'process.env': {}
-    }
-  }
 });
