ed75ab961b09e7c5f5d9e958622a148cfbcded67
johanandu <janek.durkowski@gmail.com>
2025-12-27 12:12:41 +0100

    gotujemy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,15 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: true,
     }),
   ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
-  // TU BYŁA SEKCJA EXPERIMENTAL - USUNIĘTA, BO W ASTRO 5 TO STANDARD
-  vite: {
-    define: { 'process.env': {} } // Fix dla Stripe/React
-  }
+  // USUNĄŁEM SEKCJĘ VITE, KTÓRA BLOKOWAŁA STRIPE'A
 });
